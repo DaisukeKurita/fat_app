b4640368434d7cb79e9b28e5533728e89241ac43
栗田大輔 <daikons.msn36@gmail.com>
2021-11-13 10:37:16 +0900

    Fat Modelの解消方法1

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,5 @@
 class Blog < ApplicationRecord
-  # def self.latest(number)
-  #   order(created_at: :desc).limit(number)
-  # end
-  scope :latest, -> (number = 3){order(created_at: :desc).limit(number)}
-
-  def title_change
-    update(title: 'changed')
-  end
+  include CommonModule
+  test_puts
+  validates_with BlogValidator
 end
